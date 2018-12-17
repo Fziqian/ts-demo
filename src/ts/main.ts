471b28b5ab44aca78da1b8b0874ac10588f665bd
fzq-company <547439160@qq.com>
2018-12-17 17:48:05 +0800

    增加jsdoc

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,5 @@
-import { Util } from './util/util'
-let a = 1, b = 2
-let c = Util.add(a, b)
-console.log(c)
-document.getElementById('demo').innerHTML = c + '';
+import { Util } from "./util/util";
+let a = 1, b = 2;
+let c = Util.add(a, b);
+console.log(c);
+document.getElementById("demo").innerHTML = c + "";
--- src/ts/util/util.ts
+++ src/ts/util/util.ts
@@ -1,4 +1,13 @@
+/**
+ * 工具类
+ * @class
+ */
 export class Util {
+    /**
+     * 计算两数之和
+     * @param num1 数1
+     * @param num2 数2
+     */
     public static add(num1: number, num2: number) {
         return num1 + num2;
     }
